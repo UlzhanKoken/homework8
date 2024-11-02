f062b9126234ef267b7df8daf16403b96848c545
Көкенова Ұлжан <kokenova.ulzhan@gmail.com>
2024-11-02 11:00:21 +0500

    change css

--- style.js
+++ style.js
@@ -2,3 +2,12 @@ git init
 git add . 
 git commit 
 git remote add origin https://github.com/UlzhanKoken/homework8.git
+git push 
+
+
+
+----
+git add . 
+git commit 
+git remote add origin https://github.com/UlzhanKoken/homework8.git
+git push 
